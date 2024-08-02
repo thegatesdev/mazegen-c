afbe35ef240b58df0a4919e9893acddb23256ac8
thegatesdev <mtkarels@outlook.com>
2024-08-02 21:23:47 +0200

    feat: stub wallgrid impl

---
+++ src/headers/wallgrid.h
@@ -0,0 +1,47 @@
+#ifndef WALLGRID_H
+#define WALLGRID_H
+
+#include <stdint.h>
+#include "bitset.h"
+
+#define WallGridSize(w,h) ((w + 1) * (h + 1))
+
+typedef unsigned int wallgrid_size;
+typedef uint_fast32_t wallgrid_index;
+
+typedef struct WallGrid
+{
+    wallgrid_size width;
+    wallgrid_size height;
+    Bitset *walls;
+} WallGrid;
+
+typedef enum WallDir{
+    UP, RIGHT, DOWN, LEFT
+} WallDir;
+
+typedef enum WallState{
+    EMPTY, WALL, OOB
+} WallState;
+
+WallGrid wallgrid_create_filled(wallgrid_size width, wallgrid_size height);
+
+void wallgrid_destroy(WallGrid grid);
+
+wallgrid_index wallgrid_locate(WallGrid grid, wallgrid_size x, wallgrid_size y);
+
+int wallgrid_is_edge(WallGrid grid, wallgrid_index index);
+
+int wallgrid_is_valid(WallGrid grid, wallgrid_index index, WallDir dir);
+
+void wallgrid_break(WallGrid grid, wallgrid_index index, WallDir dir);
+
+WallState wallgrid_state(WallGrid grid, wallgrid_index index, WallDir dir);
+
+wallgrid_index wallgrid_move(WallGrid grid, wallgrid_index index, WallDir dir);
+
+void wallgrid_break(WallGrid grid, wallgrid_index index, WallDir dir);
+
+void wallgrid_place(WallGrid grid, wallgrid_index index, WallDir dir);
+
+#endif // WALLGRID_H
---
+++ src/impl/wallgrid.c
@@ -0,0 +1,63 @@
+#include "wallgrid.h"
+
+WallGrid wallgrid_create_filled(wallgrid_size width, wallgrid_size height)
+{
+    WallGrid grid = {
+        .width = width,
+        .height = height,
+        .walls = bitset_create_filled(WallGridSize(width, height), ~0),
+    };
+    return grid;
+}
+
+void wallgrid_destroy(WallGrid wGrid)
+{
+    bitset_destroy(wGrid.walls);
+}
+
+inline wallgrid_index wallgrid_locate(WallGrid grid, wallgrid_size x, wallgrid_size y)
+{
+    return x + y * grid.width;
+}
+
+int wallgrid_is_edge(WallGrid grid, wallgrid_index index)
+{
+    return 0;
+}
+
+int wallgrid_is_valid(WallGrid grid, wallgrid_index index, WallDir dir)
+{
+    switch (dir)
+    {
+    case UP:
+        return index >= grid.width;
+    case RIGHT:
+        return (index % grid.width) + 1 < grid.width;
+    case DOWN:
+        return index < grid.width * (grid.height - 1);
+        /*
+        Possible optimization from (index / grid.width) + 1 < grid.height
+        if dividing is more expensive.
+        */
+    case LEFT:
+        return (index % grid.width) > 0;
+    }
+}
+
+WallState wallgrid_state(WallGrid grid, wallgrid_index index, WallDir dir)
+{
+    return 0;
+}
+
+wallgrid_index wallgrid_move(WallGrid grid, wallgrid_index index, WallDir dir)
+{
+    return 0;
+}
+
+void wallgrid_break(WallGrid grid, wallgrid_index index, WallDir dir)
+{
+}
+
+void wallgrid_place(WallGrid grid, wallgrid_index index, WallDir dir)
+{
+}
