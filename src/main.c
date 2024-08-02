650780c314a170486a423e1c47627a38a2c0afe1
thegatesdev <mtkarels@outlook.com>
2024-08-02 21:24:04 +0200

    test: wallgrid valid tests

--- src/main.c
+++ src/main.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
-#include "bitset.h"
+#include "wallgrid.h"
 
 int main(int, char **)
 {
-    Bitset *bitset = bitset_create_filled(200, ~0);
+    WallGrid grid = wallgrid_create_filled(20, 10);
 
-    
+    printf("%d\n", wallgrid_is_valid(grid, wallgrid_locate(grid, 0, 0), UP));
+    printf("%d\n", wallgrid_is_valid(grid, wallgrid_locate(grid, 0, 0), RIGHT));
+    printf("%d\n", wallgrid_is_valid(grid, wallgrid_locate(grid, 0, 0), DOWN));
+    printf("%d\n", wallgrid_is_valid(grid, wallgrid_locate(grid, 0, 0), LEFT));
 
-    bitset_destroy(bitset);
+    printf("%d\n", wallgrid_is_valid(grid, wallgrid_locate(grid, 19, 0), UP));
+    printf("%d\n", wallgrid_is_valid(grid, wallgrid_locate(grid, 19, 0), RIGHT));
+    printf("%d\n", wallgrid_is_valid(grid, wallgrid_locate(grid, 19, 0), DOWN));
+    printf("%d\n", wallgrid_is_valid(grid, wallgrid_locate(grid, 19, 0), LEFT));
+
+    printf("%d\n", wallgrid_is_valid(grid, wallgrid_locate(grid, 19, 9), UP));
+    printf("%d\n", wallgrid_is_valid(grid, wallgrid_locate(grid, 19, 9), RIGHT));
+    printf("%d\n", wallgrid_is_valid(grid, wallgrid_locate(grid, 19, 9), DOWN));
+    printf("%d\n", wallgrid_is_valid(grid, wallgrid_locate(grid, 19, 9), LEFT));
+
+    wallgrid_destroy(grid);
     return 0;
 }
